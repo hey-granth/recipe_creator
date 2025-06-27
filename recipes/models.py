0500f0ec38854096194e5694957bdc4bc4ab4729
Granth Agarwal <heygranth@gmail.com>
2025-06-27 10:56:50 +0530

    fixed bugs, made migrations and added 20+ sample recipes

--- recipes/models.py
+++ recipes/models.py
@@ -6,7 +6,7 @@ class Ingredient(models.Model):
     A model representing an ingredient, including its name, quantity, unit, allergens, and cost per unit.
     """
 
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
     unit = models.CharField(max_length=50, blank=True, null=True)
     allergens = models.CharField(max_length=200, blank=True, null=True)
